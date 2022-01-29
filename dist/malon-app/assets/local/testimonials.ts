778a24803e0970916f867310bc092edff436bc68
AndyWams <andrew.wams@gmail.com>
2022-01-29 18:43:49 +0100

    update

---
+++ dist/malon-app/assets/local/testimonials.ts
@@ -0,0 +1,82 @@
+export const Testimonials = [
+  {
+    id: 1,
+    name: 'Regina Miles',
+    avatar: 'avatar-1',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Designer',
+  },
+  {
+    id: 2,
+    name: 'Adam Allen',
+    avatar: 'avatar-2',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Marketer',
+  },
+  {
+    id: 3,
+    name: 'Bernice Bright',
+    avatar: 'avatar-3',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Jack Operator',
+  },
+  {
+    id: 4,
+    name: 'Cardice Carol',
+    avatar: 'avatar-4',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Designer',
+  },
+  {
+    id: 5,
+    name: 'Thomas King',
+    avatar: 'avatar-5',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Developer',
+  },
+  {
+    id: 6,
+    name: 'Adam Allen',
+    avatar: 'avatar-6',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Marketer',
+  },
+  {
+    id: 7,
+    name: 'Bernice Bright',
+    avatar: 'avatar-1',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Jack Operator',
+  },
+  {
+    id: 8,
+    name: 'Cardice Carol',
+    avatar: 'avatar-2',
+    comment: `Slate helps you see how many 
+      more days you need to work 
+      to reach your financial goal for
+       the month and year.`,
+    role: 'Designer',
+  },
+];
